fb64d7651ce4cf8ede7878643893fc30cf49bebc
agrynov <150433601+agrynov@users.noreply.github.com>
2025-10-20 15:22:29 +0300

    Clean repository - keep only StepProject folder

--- EKS/backend.tf
+++
@@ -1,8 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "agrynov-terraform-state"
-#     key            = "eks/terraform.tfstate"
-#     region         = "eu-central-1"
-#     encrypt        = true
-#   }
-# }
